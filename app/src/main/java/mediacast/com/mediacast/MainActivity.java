6f7322f45112aeb0e3da475ef4bce37c31efb797
davidblervaque <davidblervaque@users.noreply.github.com>
2016-02-11 12:00:43 +0100

    Test push/pull

--- app/src/main/java/mediacast/com/mediacast/MainActivity.java
+++ app/src/main/java/mediacast/com/mediacast/MainActivity.java
@@ -3,6 +3,8 @@ package mediacast.com.mediacast;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//TODO : L'application est à faire
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
