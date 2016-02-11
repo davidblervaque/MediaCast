574e5149e2953eff4790c5a557afcc05df211a3e
Bastien Fulachier <bastien@riposte-consulting.fr>
2016-02-11 16:48:53 +0100

    Creation of the menu

--- app/src/main/java/mediacast/com/mediacast/MainActivity.java
+++ app/src/main/java/mediacast/com/mediacast/MainActivity.java
@@ -1,15 +1,103 @@
 package mediacast.com.mediacast;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-//TODO : L'application est à faire
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        nvDrawer = (NavigationView) findViewById(R.id.nvView);
+        setupDrawerContent(nvDrawer);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        Fragment fragment = null;
+
+        Class fragmentClass;
+        switch (menuItem.getItemId()) {
+            case R.id.home_fragment:
+                Log.d("CLICK", "Home clicked");
+                //fragmentClass = FirstFragment.class;
+                break;
+            case R.id.artists:
+                Log.d("CLICK", "artists clicked");
+                //fragmentClass = SecondFragment.class;
+                break;
+            case R.id.albums:
+                Log.d("CLICK", "albums clicked");
+                //fragmentClass = ThridFragment.class;
+                break;
+            case R.id.videos:
+                Log.d("CLICK", "videos clicked");
+                //fragmentClass = FourthFragment.class;
+                break;
+            case R.id.all_music:
+                Log.d("CLICK", "all music clicked");
+                //fragmentClass = FifthFragment.class;
+                break;
+            default:
+                Log.d("CLICK", "default clicked");
+                //fragmentClass = FirstFragment.class;
+        }
+
+        /*try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+
+        //FragmentManager fragmentManager = getSupportFragmentManager();
+        //fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        //menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        mDrawer.closeDrawers();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
     }
 }
